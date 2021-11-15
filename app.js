e9e2e84b89265b331994a849771a1016a0472a26
emisiorski <90798919+emisiorski@users.noreply.github.com>
2021-11-15 17:37:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
 const printProfileData = (profileDataArr) => {
-  for (ket i = 0; i <profileDataArr.length; i++) {
+  for (let i = 0; i <profileDataArr.length; i++) {
   console.log(profileDataArr);
 };
 
